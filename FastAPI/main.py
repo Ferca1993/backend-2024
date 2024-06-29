168120ec041212dd8151a5eaba9e29680a68da5e
Ferca1993 <126630170+Ferca1993@users.noreply.github.com>
2024-06-29 10:49:33 -0600

    I started off FastAPI and built some functions

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -5,13 +5,17 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
+ # upload the page
+# raw python
+# Hello FastAPI
+# To begin with, We're going to create a function so-called it "root" and it must be async in oder to work well
+@app.get("/") # get name "/"
+async def  root():
+    return "Hi, FastApi"
 
+@app.get("/url") # get name "/url"
+async def  url():
+    return {"course url":"http//mouredev.com/python"}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+#PS C:\Users\ferna\Desktop\backend_24\FastAPI> uvicorn main:app --reload to run our app
+#to call each element into Fast PI we must call it for its "get name"

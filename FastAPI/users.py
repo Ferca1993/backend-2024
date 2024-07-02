0d85eb505f2027ae50243889cd1075a4671cc992
Ferca1993 <126630170+Ferca1993@users.noreply.github.com>
2024-07-02 11:36:52 -0600

    I learned how to get request by path and by query

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -16,12 +16,13 @@ app = FastAPI()
 # Entity users
 
 class User(BaseModel): # base model is a tool which aids us out to build a class into a easy way
+    id : int
     name: str
     surname: str
     url: str
     age: int
 
-user_list = [User(name = "Ivan",surname ="Calvillo",url = "https://www.facebook.com/ivanfernando.calvillosanagustin/,", age = 30)] # It might be a database
+users_list = [User(id = 1 ,name = "Ivan",surname ="Calvillo",url = "https://www.facebook.com/ivanfernando.calvillosanagustin/,", age = 30),User(id = 2, name = "Fersh",surname ="San Agustin",url = "https://www.facebook.com/ivanfernando.calvillosanagustin/,", age = 31)] # It might be a database
 
 # json manual
 @app.get("/usersjson") 
@@ -29,6 +30,28 @@ async def  usersjson():
     return {"name":"Ivan","surname": "Calvillo","url": "https://www.facebook.com/ivanfernando.calvillosanagustin/"}
 
 # json from a base model
-@app.get("/user_class") 
-async def  user_class():
-    return user_list
+@app.get("/users_class") 
+async def  users_class():
+    return users_list
+
+def search_user(id: int):
+    users = filter(lambda user:user.id == id, users_list )
+    try:
+        return list(users)[0]
+    except:
+        return "Error, the user weren't found "
+        #http://127.0.0.1:8000/user_query/?id=1 user by query
+
+
+# path model
+@app.get("/user/{id}") # In this fucntion is We can give parameters to it, I t means, This case We're gonna seeking out a user by its id
+async def  user_id(id: int):
+    return search_user(id)
+    
+
+# query model
+@app.get("/user/") # "Calling a user by their ID means accessing them through bounded data."
+async def  user_query(id: int, name: str):
+    return search_user(id)
+
+
